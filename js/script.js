878477494fa662d59abf59ab1228b19a4e590571
Jovan Lanutan <jovanlanutan2001@Gmail.com>
2022-12-05 21:00:42 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const email = document.querySelector('#email');
+const btn = document.querySelector('#btn');
+
+// Validate email
+// https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+const isEmailValid = input => {
+  const email = String(input.value)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+
+  if (email !== null) {
+    displaySuccess(input);
+  } else {
+    displayError(input, 'Please provide a valid email address');
+  }
+};
+
+// Display error
+const displayError = (input, err) => {
+  const inputParent = input.parentElement;
+  inputParent.className = 'main__input error';
+  const errMessage = document.querySelector('#required-email');
+  errMessage.textContent = err;
+};
+
+// Display success
+const displaySuccess = input => {
+  const inputParent = input.parentElement;
+  inputParent.className = 'main__input success';
+  input.value = '';
+};
+
+btn.addEventListener('click', e => {
+  e.preventDefault();
+  isEmailValid(email);
+});
